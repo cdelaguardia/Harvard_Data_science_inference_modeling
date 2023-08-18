33d232d34a87d41b9e3557e5cff192e00d7fbc5f
cdelaguardia <cdelaguardia1987@gmail.com>
2023-08-18 06:48:14 -0600

    Answering question 2

--- Brexit_poll_analysis.R
+++ Brexit_poll_analysis.R
@@ -33,3 +33,22 @@ print(d <- 2*p - 1)
 #of “Remain” voters and “Leave” voters?
 print(stand_error_d <- 2*stand_error_x_hat)
 
+#What is the average of the observed spreads (spread)?
+brexit_poll_hat <- brexit_polls %>% mutate(x_hat = (spread + 1)/2)
+mean(brexit_poll_hat$spread)
+
+#What is the standard deviation of the observed spreads?
+sd(brexit_poll_hat$spread)
+
+#What is the average of x_hat, the estimates of the parameter
+mean(brexit_poll_hat$x_hat)
+
+#What is the standard deviation of x_hat?
+sd(brexit_poll_hat$x_hat)
+
+#Use qnorm() to compute the 95% confidence interval for 𝑋̂.
+#What is the lower bound of the 95% confidence interval?
+N <- brexit_poll_hat$samplesize[1]
+stand_error_x_hat <- sqrt(brexit_poll_hat$x_hat[1]*(1 - brexit_poll_hat$x_hat[1])/N)
+conf_interval_x_hat <- c(brexit_poll_hat$x_hat[1] - qnorm(0.975)*stand_error_x_hat, brexit_poll_hat$x_hat[1] + qnorm(0.975)*stand_error_x_hat)
+
